8f02115b54883599c2599822c3550047e125b984
aldlss <ayaldlss@gmail.com>
2022-03-06 12:33:32 +0800

    修复了输出的字符串显示图片时不正常的问题 主要是通道不对，深度也不对 目前生成的字符串是RGB565下的表示，为16位1通道

--- main.cpp
+++ main.cpp
@@ -19,35 +19,43 @@ int main()
 	}
 	Mat imgdst = img;
 	int xx, yy;
-	puts("please input the pic size you want.(input 0 0 is the original size)");
+	puts("please input the pic size you want.(0 0 is the original size)");
 	if (scanf_s("%d %d", &xx, &yy) != 2)
 	{
 		puts("read size fail");
 		system("pause");
 		return -2;
 	}
-	if (xx && yy)
-	{
-		Size dssize(xx, yy);
-		resize(img, imgdst, dssize);
-	}
+	if (!xx)xx = img.rows;
+	if (!yy)yy = img.cols;
+	Size dssize(xx, yy);
+	resize(img, imgdst, dssize);
 	int position = file.find_last_of('.');
+	cvtColor(imgdst, img, COLOR_BGR2BGR565);
 	file.erase(position);
 	file += ".txt";
 	if (img.isContinuous())
 	{
 		FILE* f;
 		freopen_s(&f, file.c_str(), "w", stdout);
-		int row = imgdst.rows, col = imgdst.cols;
-		uchar* str[] = { imgdst.data };
+		int row = img.rows, col = img.cols, step = img.step,channel=img.channels();
+		int size = row * col;
+		ushort bitt;
 		for (int i = 0; i < row; ++i)
 		{
-			for (int i = 0; i < col; ++i)printf("0x%04x, ", str[0][i]);
+			for(int j = 0; j < col; ++j)
+			{
+			/*	实际上，两个通道，第一个通道是低八位，第二个通道是高八位，然后是按照RGB565来排布的
+				所以上面那里转化写的转化成BGR565疑似纯属忽悠了	*/
+				bitt = ushort(img.data[i * step + j * channel]);
+				bitt |= ushort(img.data[i * step + j * channel + 1])<<8;
+
+				printf("0x%04x, ", bitt);
+			}
 			printf("\n");
 		}
 		freopen_s(&f, "CON", "w", stdout);
 		puts("succeed");
-		fclose(f);
 	}
 	else puts("some error has happen,please try again minutes later");
 	system("pause");
